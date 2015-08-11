d3932ce38571016b72d95e94af1fb198bcb3a89f
brettkan <bkfighton@gmail.com>
2015-08-10 22:21:36 -0700

    attempt maximumProductSubarray

--- maximumProductSubarray/maximumProductSubarray.js
+++ maximumProductSubarray/maximumProductSubarray.js
@@ -14,5 +14,24 @@ the contiguous subarray [2,3] has the largest product = 6.
  * @return {number}
  */
 var maxProduct = function(nums) {
-  
+  var max = nums[0];
+  var lowIndex = 0;
+  var highIndex = 0;
+
+  var arrProduct = function(aggregator, number) {
+    return aggregator *= number;
+  };
+
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j <= nums.length; j++) {
+      var current = nums.slice(i, j).reduce(arrProduct, 1);
+      if (current > max) {
+        max = current;
+        lowIndex = i;
+        highIndex = j;
+      }
+    }
+  }
+
+  return nums.slice(lowIndex, highIndex);
 };
