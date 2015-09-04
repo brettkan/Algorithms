dd34c1e95cbb6fd7d180ac92de4a01ba6e07a5a9
brettkan <bkfighton@gmail.com>
2015-09-04 11:34:07 -0700

    add condensedRanges

---
+++ condensedRanges/condensedRanges.js
@@ -0,0 +1,20 @@
+/* 
+
+Write a function condensedRanges that takes an array of 
+ranges and returns an array of condensed ranges.
+
+Example: 
+
+var ranges =   [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+condensedRanges(ranges);
+
+
+Should return:
+
+[[0, 1], [3, 8], [9, 12]]
+
+*/
+
+var condensedRanges = function(ranges) {
+
+};
