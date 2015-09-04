61076e6c1591995118ad12bf5d642aef124a525b
brettkan <bkfighton@gmail.com>
2015-09-03 22:50:05 -0700

    complete first portion wordsInMatrix

--- wordsInMatrix/wordsInMatrix.js
+++ wordsInMatrix/wordsInMatrix.js
@@ -6,11 +6,11 @@ matrix that are in the dictionary. (Going across, down or diagonally).
 
 var dictionary = {
   house: true,
-  brand: true
+  brand: true,
   bros: true,
   man: true,
   on: true,
-  cry: true
+  cry: true,
   urn: true,
   ball: false,
   crow: false
@@ -27,5 +27,62 @@ var matrix = [
 */
 
 var wordsInMatrix = function(dictionary, matrix) {
-  
+  var matrixColumns = matrix.length;
+  var matrixRows = matrix[0].length;
+  var result = {};
+
+  var checkRight = function(startColumn, startRow) {
+    var wordBuild = '';
+
+    for (var i = startRow; i < matrixRows; i++) {
+      wordBuild += matrix[startColumn][i];
+      if (wordBuild in dictionary) {
+        result[wordBuild] = true;
+      }
+    }
+  };
+
+  var checkLeft = function(startColumn, startRow) {
+    var wordBuild = '';
+
+    for (var i = startRow; i >= 0; i--) {
+      wordBuild += matrix[startColumn][i];
+      if (wordBuild in dictionary) {
+        result[wordBuild] = true;
+      }
+    }
+  };
+
+  var checkDown = function(startColumn, startRow) {
+    var wordBuild = '';
+
+    for (var i = startColumn; i < matrixColumns; i++) {
+      wordBuild += matrix[i][startRow];
+      if (wordBuild in dictionary) {
+        result[wordBuild] = true;
+      }
+    }
+  };
+
+  var checkUp = function(startColumn, startRow) {
+    var wordBuild = '';
+
+    for (var i = startColumn; i >= 0; i--) {
+      wordBuild += matrix[i][startRow];
+      if (wordBuild in dictionary) {
+        result[wordBuild] = true;
+      }
+    }
+  };
+
+  for (var i = 0; i < matrixColumns; i++) {
+    for (var j = 0; j < matrixRows; j++) {
+      checkRight(i, j);
+      checkLeft(i, j);
+      checkDown(i, j);
+      checkUp(i, j);
+    }
+  }
+
+  return result;
 };
