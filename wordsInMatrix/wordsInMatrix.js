8060335b150710c5c7ac2b200ec19abe41c22727
brettkan <bkfighton@gmail.com>
2015-09-03 21:56:37 -0700

    add wordsInMatrix

---
+++ wordsInMatrix/wordsInMatrix.js
@@ -0,0 +1,31 @@
+/* 
+
+Given a dictionary, and a matrix of letters, find all the words in the 
+matrix that are in the dictionary. (Going across, down or diagonally).
+
+
+var dictionary = {
+  house: true,
+  brand: true
+  bros: true,
+  man: true,
+  on: true,
+  cry: true
+  urn: true,
+  ball: false,
+  crow: false
+}
+
+var matrix = [
+  ['b', 'b', 'c', 'h'],
+  ['u', 'r', 'n', 'o'],
+  ['y', 'a', 'o', 'u'],
+  ['m', 'n', 'o', 's'],
+  ['q', 'd', 's', 'e']
+]
+
+*/
+
+var wordsInMatrix = function(dictionary, matrix) {
+  
+};
