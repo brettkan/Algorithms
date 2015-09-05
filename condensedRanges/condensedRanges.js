824352c534f0256ce70d746a4c431a69b228eeb8
brettkan <bkfighton@gmail.com>
2015-09-04 18:52:29 -0700

    complete condensedRanges

--- condensedRanges/condensedRanges.js
+++ condensedRanges/condensedRanges.js
@@ -16,5 +16,24 @@ Should return:
 */
 
 var condensedRanges = function(ranges) {
+  var sortedRanges = ranges.slice().sort(function(a, b) {
+    return a[0] - b[0];
+  });
 
+  var condensed = [];
+  var prev = sortedRanges[0].slice();
+
+  for (var i = 1; i < sortedRanges.length; i++) {
+    var current = sortedRanges[i];
+    if (prev[1] >= current[0]) {
+      prev[1] = Math.max(prev[1], current[1]);
+    } else {
+      condensed.push(prev);
+      prev = current;
+    }
+  }
+
+  condensed.push(prev);
+
+  return condensed;
 };
