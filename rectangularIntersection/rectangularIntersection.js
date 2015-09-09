0b817a20b4503cafce2f4e0862331f3b27790c7a
brettkan <bkfighton@gmail.com>
2015-09-09 11:47:25 -0700

    add rectangularIntersection

---
+++ rectangularIntersection/rectangularIntersection.js
@@ -0,0 +1,23 @@
+/* 
+
+Write a function to find the rectangular intersection of two given rectangles.
+
+  my_rectangle = {
+
+    # coordinates of bottom-left corner:
+    'x': 2,
+    'y': 6,
+
+    # width and height
+    'width': 11,
+    'height': 5,
+
+  }
+
+*/
+
+var rectangularIntersection = function(rect1, rect2) {
+
+};
+
+
