ffacfcc51e3a2a0fda8eae923d6acdf15a0ebd2a
brettkan <bkfighton@gmail.com>
2015-09-14 09:21:31 -0700

    add highestProductOfThree

---
+++ highestProductOfThree/highestProductOfThree.js
@@ -0,0 +1,10 @@
+/* 
+
+Given an arrayOfInts, find the highestProduct you can get from three of the integers.
+The input arrayOfInts will always have at least three integers.
+
+*/
+
+var highestProduct = function(arrayOfInts) {
+  
+};
