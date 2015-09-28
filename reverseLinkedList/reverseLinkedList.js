02f8eae86d6078cb0ad56c225ea3f2b69b7167d0
brettkan <bkfighton@gmail.com>
2015-09-28 09:51:27 -0700

    add reverseLinkedList

---
+++ reverseLinkedList/reverseLinkedList.js
@@ -0,0 +1,31 @@
+/* 
+
+Write a function for reversing a linked list.
+
+Your function will have one input: the head of the list.
+
+Your function should return the new head of the list.
+
+example: 
+
+var nodeA = new Node('A');
+var nodeB = new Node('B');
+var nodeC = new Node('C');
+var nodeD = new Node('D');
+nodeA.next = nodeB;
+nodeB.next = nodeC;
+nodeC.next = nodeD;
+
+reverseLinkedList(nodeA); // should return nodeD
+
+*/
+
+var Node = function(value) {
+  this.value = value;
+  this.next = null;
+};
+
+var reverseLinkedList = function(headNode) {
+
+};
+
