6037d6143949a748b4d155baa1c2d45de0e49fbc
brettkan <bkfighton@gmail.com>
2018-08-21 12:14:50 -1000

    Add implementRand10UsingRand7 problem

---
+++ implementRand10UsingRand7/implementRand10UsingRand7.js
@@ -0,0 +1,59 @@
+/*
+
+Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a
+function rand10 which generates a uniform random integer in the range 1 to 10.
+
+Do NOT use system's Math.random().
+
+
+Example 1:
+
+Input: 1
+Output: [7]
+Example 2:
+
+Input: 2
+Output: [8,4]
+Example 3:
+
+Input: 3
+Output: [8,1,10]
+ 
+
+Note:
+
+rand7 is predefined.
+Each testcase has one argument: n, the number of times that rand10 is called.
+ 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+
+*/
+
+
+/**
+ * The rand7() API is already defined for you.
+ * var rand7 = function() {}
+ * @return {number} a random integer in the range 1 to 7
+ */
+var rand10 = function() {
+    
+};
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(rand10())
+console.log(rand10())
+console.log(rand10())
