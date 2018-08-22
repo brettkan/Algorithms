e39880691f4f139bf09761dead5bd4eaf22545c8
brettkan <bkfighton@gmail.com>
2018-08-21 14:52:01 -1000

    Add solution and test cases for removeKDigits

--- removeKDigits/removeKDigits.js
+++ removeKDigits/removeKDigits.js
@@ -31,7 +31,40 @@ Explanation: Remove all the digits from the number and it is left with nothing w
  * @return {string}
  */
 var removeKdigits = function(num, k) {
-    
+    let numValidZeroes = 0;
+    let numToRemove = 0;
+    let lastZeroIndex = -1;
+    let index = 0
+
+    // If there are <= k non-zero digits on the left of a zero, remove those first
+    while (index < k + numValidZeroes + 1) {
+        if (num[index] === '0') {
+            numValidZeroes++
+            numToRemove += index - lastZeroIndex - 1
+            lastZeroIndex = index
+        }
+        index++
+    }
+
+    // Get new number and k after removing digits left of zero
+    if (numValidZeroes > 0) {
+        num = num.slice(numValidZeroes + numToRemove)
+        k -= numToRemove
+    }
+
+    // Look at first k+1 digits, and remove "k" number of highest digits
+    const highestLeftArray = num.slice(0, k + 1).split('').sort().reverse()
+    // Remove smallest digit - this one will remain
+    highestLeftArray.pop()
+
+    // Remove highest digits from num
+    while (highestLeftArray.length > 0) {
+        let digitToRemove = highestLeftArray.shift()
+        let indexToRemove = num.indexOf(digitToRemove)
+        num = num.slice(0, indexToRemove) + num.slice(indexToRemove + 1)
+    }
+
+    return num || '0'
 };
 
 
@@ -45,6 +78,11 @@ var removeKdigits = function(num, k) {
  * TEST CASES
  **/
 
-console.log(removeKdigits(1432219, 3))
-console.log(removeKdigits(10200, 1))
-console.log(removeKdigits(10, 2))
+console.log(removeKdigits('1432219', 3), 'and expected: 1219')
+console.log(removeKdigits('2403012', 1), 'and expected: 203012')
+console.log(removeKdigits('2403012', 2), 'and expected: 3012')
+console.log(removeKdigits('2403012', 3), 'and expected: 12')
+console.log(removeKdigits('2403012', 4), 'and expected: 1')
+console.log(removeKdigits('10200', 1), 'and expected: 200')
+console.log(removeKdigits('10200', 2), 'and expected: 0')
+console.log(removeKdigits('10', 2), 'and expected: 0')
