4441d87249ea944e1a2cf147d08685087db60b13
brettkan <bkfighton@gmail.com>
2018-08-22 18:24:59 -0700

    Add subsetsII problem

---
+++ subsetsII/subsetsII.js
@@ -0,0 +1,42 @@
+/*
+
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: [1,2,2]
+Output:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    
+};
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(subsetsWithDup([1, 2, 2]))
