2c2031beea8a5cd7c7a191e3ed98ea11d11ae655
brettkan <bkfighton@gmail.com>
2018-08-24 13:01:03 -0700

    Add solution for subsetsII

--- subsetsII/subsetsII.js
+++ subsetsII/subsetsII.js
@@ -25,8 +25,25 @@ Output:
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    
+    const sorted = nums.sort()
+    return [[], ...backtrack(nums, 0, [])]
 };
+
+function backtrack(nums, index, current) {
+    const solutions = []
+
+    for (let i = index; i < nums.length; i++) {
+        if (i !== index && nums[i] === nums[i - 1]) {
+            continue
+        }
+
+        current.push(nums[i])
+        solutions.push([...current], ...backtrack(nums, i + 1, current))
+        current.pop()
+    }
+
+    return solutions
+}
 
 
 /**
@@ -39,4 +56,6 @@ var subsetsWithDup = function(nums) {
  * TEST CASES
  **/
 
+console.log(subsetsWithDup([1, 2, 3]))
 console.log(subsetsWithDup([1, 2, 2]))
+console.log(subsetsWithDup([1, 1, 1, 2, 2, 2]))
