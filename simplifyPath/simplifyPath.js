688b2394415ee6bbac6dd5e247cd50ac91b69330
brettkan <bkfighton@gmail.com>
2018-08-24 13:19:59 -0700

    Add simplifyPath problem

---
+++ simplifyPath/simplifyPath.js
@@ -0,0 +1,39 @@
+/*
+
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+
+Corner Cases:
+
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+
+*/
+
+
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+
+};
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(simplifyPath("/home/"), 'and the answer is: /home')
+console.log(simplifyPath("/a/./b/../../c/"), 'and the answer is: /c')
