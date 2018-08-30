c2f037d2cef2499a87bf77eddcb7172455366871
brettkan <bkfighton@gmail.com>
2018-08-30 09:24:50 -0700

    Add initial solution for LRUcache

--- LRUcache/LRUcache.js
+++ LRUcache/LRUcache.js
@@ -29,7 +29,9 @@ cache.get(4);       // returns 4
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-    
+    this.capacity = capacity
+    this.queue = []
+    this.indexes = {}
 };
 
 /** 
@@ -37,7 +39,15 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    
+    if (!this.indexes.hasOwnProperty(key)) {
+        return -1
+    }
+
+    const selectedPair = this.indexes[key]
+    const queueIndex = this.queue.indexOf(selectedPair)
+    this.queue.splice(queueIndex, 1)
+    this.queue.unshift(selectedPair)
+    return selectedPair[1]
 };
 
 /** 
@@ -46,7 +56,22 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    
+    if (this.indexes.hasOwnProperty(key)) {    
+        const queueIndex = this.queue.indexOf(this.indexes[key])
+        this.queue.splice(queueIndex, 1)
+        this.queue.unshift(this.indexes[key])
+        this.indexes[key][1] = value
+    } else {
+        if (this.queue.length === this.capacity) {
+            const keyToRemove = this.queue[this.queue.length - 1][0]
+            this.queue.pop()
+            delete this.indexes[keyToRemove]
+        }
+
+        const pair = [key, value]
+        this.queue.unshift(pair)
+        this.indexes[key] = pair
+    }
 };
 
 /** 
@@ -69,12 +94,12 @@ LRUCache.prototype.put = function(key, value) {
 
 const cache = new LRUCache(2); // pass in the capacity
 
-cache.put(1, 1);
-cache.put(2, 2);
-cache.get(1);       // returns 1
-cache.put(3, 3);    // evicts key 2
-cache.get(2);       // returns -1 (not found)
-cache.put(4, 4);    // evicts key 1
-cache.get(1);       // returns -1 (not found)
-cache.get(3);       // returns 3
-cache.get(4);       // returns 4
+console.log(cache.put(1, 1));
+console.log(cache.put(2, 2));
+console.log(cache.get(1));       // returns 1
+console.log(cache.put(3, 3));    // evicts key 2
+console.log(cache.get(2));       // returns -1 (not found)
+console.log(cache.put(4, 4));    // evicts key 1
+console.log(cache.get(1));       // returns -1 (not found)
+console.log(cache.get(3));       // returns 3
+console.log(cache.get(4));       // returns 4
