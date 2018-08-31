924391989e9705410b07960eec6169b3efe8418e
brettkan <bkfighton@gmail.com>
2018-08-30 17:44:34 -0700

    Add solution for containerWithMostWater

--- containerWithMostWater/containerWithMostWater.js
+++ containerWithMostWater/containerWithMostWater.js
@@ -18,7 +18,36 @@ Output: 49
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let i = 0
+    let j = height.length - 1
+    let maxArea = 0
+
+    while (i !== j) {
+        maxArea = Math.max(maxArea, findArea(height, i, j))
+
+        if (height[i] > height[j]) {
+            j--
+        } else {
+            i++
+        }
+    }
+
+    return maxArea
 };
+
+function findArea(arr, begIndex, endIndex) {
+    return (endIndex - begIndex) * Math.min(arr[begIndex], arr[endIndex])
+}
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
 
 console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
