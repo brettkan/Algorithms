6bff2508846442847419fc2a90a1f30e5f229720
brettkan <bkfighton@gmail.com>
2018-08-30 19:37:30 -0700

    Fix extraneous checking

--- arithmeticBoggle/arithmeticBoggle.js
+++ arithmeticBoggle/arithmeticBoggle.js
@@ -1,6 +1,17 @@
 /*
 
-EXPLANATION HERE
+Write a function that takes a magic number and a list of numbers. It returns true if it can
+insert add or subtract operations in the list of numbers to get the magic number. Otherwise,
+it returns false.
+
+For example:
+
+f(10, [1, 2]) = false. There's no way to add or subtract 1 and 2 to get 10.
+f(2, [1, 2, 3, 4]) = true. 1 + 2 + 3 - 4 = 2.
+f(0, []) = true
+f(1, []) = false
+f(1, [1]) = true
+f(0, [1]) = false
 
 */
 
@@ -16,15 +27,13 @@ function arithmeticBoggle(magicNumber, numbers) {
         for (let i = 0; i < options.length; i++) {
             let curNumber = options[i]
 
-            options.splice(i, 1)
-            if (attemptSolution(target, options, current + curNumber)) {
+            if (attemptSolution(target, options.slice(i + 1), current + curNumber)) {
                 return true
             }
 
-            if (attemptSolution(target, options, current - curNumber)) {
+            if (attemptSolution(target, options.slice(i + 1), current - curNumber)) {
                 return true
             }
-            options.splice(i, 0, curNumber)
         }
 
         return false
@@ -49,5 +58,5 @@ console.log(arithmeticBoggle(0, [])) // = true
 console.log(arithmeticBoggle(1, [])) // = false
 console.log(arithmeticBoggle(1, [1])) // = true
 console.log(arithmeticBoggle(0, [1])) // = false
-console.log(arithmeticBoggle(-5, [4, 3, 2])) // = true
+console.log(arithmeticBoggle(-1, [4, 3, 1])) // = true
 
