c90e8d73e7a46be40df3c7e0575d1ba6cdb8b085
brettkan <bkfighton@gmail.com>
2018-09-06 16:15:46 -0700

    Add integerToEnglishWords

---
+++ integerToEnglishWords/integerToEnglishWords.js
@@ -0,0 +1,51 @@
+/*
+
+Convert a non-negative integer to its english words representation. Given input is guaranteed
+to be less than 2^31 - 1.
+
+Example 1:
+
+Input: 123
+Output: "One Hundred Twenty Three"
+
+Example 2:
+
+Input: 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+
+Example 3:
+
+Input: 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+
+Example 4:
+
+Input: 1234567891
+Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+
+*/
+
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var numberToWords = function(num) {
+    
+};
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(numberToWords(123), 'for 123')
+console.log(numberToWords(12345), 'for 12345')
+console.log(numberToWords(1234567), 'for 1234567')
+console.log(numberToWords(1234567891), 'for 1234567891')
