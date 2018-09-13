635d827b7912047037f20a618c8c2ce9300499a7
brettkan <bkfighton@gmail.com>
2018-09-13 10:10:59 -0700

    Add implementMove2048

---
+++ implementMove2048/implementMove2048.js
@@ -0,0 +1,36 @@
+/*
+
+Implement a move for the game 2048
+
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var moveRight = function(grid) {
+    
+};
+
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(moveRight([
+    [2, 2, 2, 2],
+    [6, 8, 4, 2],
+    [null, 4, 4, null],
+    [2, null, null, 2],
+]))
+
+// [null, null, 4, 4]
+// [6, 8, 4, 2]
+// [null, null, null, 8]
+// [null, null, null, 4]
