109d12ab5ca4f51cc2923361b858b419b1d3b9be
brettkan <bkfighton@gmail.com>
2018-09-16 17:25:42 -0700

    Fix spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -14,7 +14,7 @@ function spiralTraversal(grid) {
     let topBound = 0
     let bottomBound = grid.length - 1
 
-    while (leftBound < rightBound && topBound < bottomBound) {
+    while (leftBound <= rightBound && topBound <= bottomBound) {
         // Iterate over from right to left across top of open grid space
         for (let i = leftBound; i <= rightBound; i++) {
             doWork(grid[topBound][i])
@@ -28,16 +28,20 @@ function spiralTraversal(grid) {
         rightBound--
 
         // Iterate from right to left across bottom of open grid space
-        for (let k = rightBound; k >= leftBound; k--) {
-            doWork(grid[bottomBound][k])
+        if (topBound <= bottomBound) {
+            for (let k = rightBound; k >= leftBound; k--) {
+                doWork(grid[bottomBound][k])
+            }
+            bottomBound--
         }
-        bottomBound--
 
-        // Iterate from bottom to top on left side of open grid space
-        for (let l = bottomBound; l >= topBound; l--) {
-            doWork(grid[l][leftBound])
+        if (leftBound <= rightBound) {
+            // Iterate from bottom to top on left side of open grid space
+            for (let l = bottomBound; l >= topBound; l--) {
+                doWork(grid[l][leftBound])
+            }
+            leftBound++
         }
-        leftBound++
     }
 
     return grid
@@ -81,4 +85,7 @@ console.log(spiralTraversal(grid3))
 
 const grid4 = createGrid(3, 5)
 console.log(spiralTraversal(grid4))
+
+const grid5 = createGrid(2, 7)
+console.log(spiralTraversal(grid5))
 
