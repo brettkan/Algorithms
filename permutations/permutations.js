1d4abdb826afe793ad91edc1c0838a1105877180
brettkan <bkfighton@gmail.com>
2018-09-18 21:13:59 -0700

    Add permutations

---
+++ permutations/permutations.js
@@ -0,0 +1,42 @@
+/*
+
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    
+};
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(permute([1,2,3]))
+
+
+
