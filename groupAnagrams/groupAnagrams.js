abb2315fbe2f94aac498b140ac6c9b6d04407dfa
brettkan <bkfighton@gmail.com>
2018-09-21 11:21:58 -0700

    Add groupAnagrams

---
+++ groupAnagrams/groupAnagrams.js
@@ -0,0 +1,43 @@
+/*
+
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+
+*/
+
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+};
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+
+
