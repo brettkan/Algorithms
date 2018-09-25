c684e65c6b0ed76afd51711d7a708984dd5b6ef5
brettkan <bkfighton@gmail.com>
2018-09-24 22:07:39 -0700

    Add solution for minimumWindowSubstring

--- minimumWindowSubstring/minimumWindowSubstring.js
+++ minimumWindowSubstring/minimumWindowSubstring.js
@@ -23,7 +23,54 @@ in S.
  * @return {string}
  */
 var minWindow = function(s, t) {
-    
+    const targetDict = t.split('').reduce((accumulator, letter) => {
+        accumulator[letter] = accumulator[letter] || 0;
+        accumulator[letter]++
+        return accumulator
+    }, {})
+    const requiredUnique = Object.entries(targetDict).length
+
+    const currentWindowDict = {}
+    let currentUnique = 0
+
+    let solution = ''
+
+    let left = 0
+    let right = 0
+
+    while (right < s.length) {
+        const currentLetter = s[right]
+        currentWindowDict[currentLetter] = currentWindowDict[currentLetter] || 0
+        currentWindowDict[currentLetter]++
+
+        if (
+            targetDict.hasOwnProperty(currentLetter) &&
+            currentWindowDict[currentLetter] === targetDict[currentLetter]
+        ) {
+            currentUnique++
+        }
+
+        while (currentUnique === requiredUnique) {
+            const currentLength = right - left + 1
+            if (solution === '' || currentLength < solution.length) {
+                solution = s.substring(left, right + 1)
+            }
+
+            currentWindowDict[s[left]]--
+            if (
+                targetDict.hasOwnProperty(s[left]) &&
+                currentWindowDict[s[left]] < targetDict[s[left]]
+            ) {
+                currentUnique--
+            }
+
+            left++
+        }
+
+        right++
+    }
+
+    return solution
 };
 
 
