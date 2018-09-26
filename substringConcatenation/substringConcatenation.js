f0d735ac5960df80aabb81dcc2dfb2867a9590dc
brettkan <bkfighton@gmail.com>
2018-09-26 11:52:11 -0700

    Add substringConcatenation

---
+++ substringConcatenation/substringConcatenation.js
@@ -0,0 +1,49 @@
+/*
+
+You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+Example 1:
+
+Input:
+  s = "barfoothefoobarman",
+  words = ["foo","bar"]
+Output: [0,9]
+Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+The output order does not matter, returning [9,0] is fine too.
+
+Example 2:
+
+Input:
+  s = "wordgoodstudentgoodword",
+  words = ["word","student"]
+Output: []
+
+*/
+
+
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function(s, words) {
+
+}
+
+/**
+ * HELPERS
+ **/
+
+
+
+/**
+ * TEST CASES
+ **/
+
+console.log(findSubstring('barfoothefoobarman', ["foo","bar"]), 'and output should be [0,9]')
+console.log(findSubstring('wordgoodstudentgoodword', ["word","student"]), 'and output should be []')
+
+
+
+
+
